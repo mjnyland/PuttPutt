0d2a7a0649bc2189ba9546a2149864f94bffb0f4
Michael <88445485+mjnyland@users.noreply.github.com>
2024-07-21 12:06:53 -0400

    first commit

---
+++ PuttPutt/PuttPuttApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuttPuttApp.swift
+//  PuttPutt
+//
+//  Created by Michael Nyland on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PuttPuttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
